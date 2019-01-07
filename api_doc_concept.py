6fdb3822df88beb198c034680ac04df5d3c3389e
Tina Kallio <tina.kallio@gmail.com>
2019-01-07 17:01:09 +0100

    Added default values etc

--- api_doc_concept.py
+++ api_doc_concept.py
@@ -1,7 +1,8 @@
 # Should both summary and description be included (for each path / method)? 
 # Should responses be defined in ApiDocs?
 # Should it be possible to document endpoints that not are api?
-# How do I include ApiDoc-info in ENDPIOINT_MAP in real varsion? How much is still needed?
+# Should I include ApiDoc-info in ENDPIOINT_MAP or in a seperate list/dict?
+# Should the current info be overwritten directly or stored for other purposes?
 
 import collections
 import cherrypy
@@ -9,7 +10,7 @@ from distutils.util import strtobool
 
 
 #-----------------in __init__----------------------
-ENDPOINT_MAP = collections.defaultdict(list)
+ENDPOINT_MAP = collections.defaultdict(list) 
 
 def ApiDoc(descr="", tag="", param=None, respons=None):
 
@@ -23,7 +24,7 @@ def ApiDoc(descr="", tag="", param=None, respons=None):
             })
 
     def _wrapper(func):
-        path = "/" + func.__name__
+        path = "/" + func.__name__ # Other solution needed for Ceph-code
         ENDPOINT_MAP[path].append({
             'summary': descr,
             'tag': tag,
@@ -39,74 +40,110 @@ def ApiDoc(descr="", tag="", param=None, respons=None):
 @ApiDoc(
     descr = "A dummy endpoint",
     param = {
-        'my_num': (int, "A number of your choice"),
-        'my_string': (str, "A dummy message")
+        'my_num': ('integer', "A number of your choice"), # int or 'integer'? str or 'string'?
+        'my_string': ('string', "A dummy message") 
     },
     respons = {
-        200: "OK", 
-        404: "Page not found"
+        '200': "OK", # Respons code as string or int?
+        '404': "Page not found"
     },
     tag = "Group A"
     )
-def test_endpoint(my_num, my_string):
+def first_endpoint(my_num, my_string):
     print(my_string)
     return my_num   
+
+@ApiDoc(
+    respons = {
+        '401': "Not the default message" 
+    },
+    tag = "Group D"
+    )
+def second_endpoint():
+    print("2nd endpoint")
+    return None   
 
 
 #-----------------in __docs.py__----------------------
 
-# To include all descriptions that should be available.
-# (Manually defined since group should be defined by the developers.)
+# Should include ALL descriptions that should be available.
+# (Manually defined since developers should be able to define their own group.)
 API_TAGS = {
     'Group A': "A dummy description for Group A",
     'Group B': "This is the description of Group B",
-    'Group C': "Group C is descibed here"
+    'Group C': "Group C is described here"
 }
 
-# Reduced version. Should control if is_api, etc.
-# Should return current values if tag="".
 def _gen_tags():
     tags = []
-    for endpoint in ENDPOINT_MAP:
-        tag = ENDPOINT_MAP[endpoint][0]['tag']
-        if tag in API_TAGS.keys():
-            tags.append({
-                'name': tag,
-                'description': API_TAGS.get(tag)
-            })
-
+    for tag in sorted(API_TAGS.keys()):
+        tags.append({
+            'name': tag, 
+            'description': API_TAGS.get(tag)
+        })
     return tags
 
-# Reduced version. Should include Parameter location, etc.
-def _gen_param(param):
+# Reduced version. Should include all default responses.
+def _gen_responses_descr(spec_respons=None):
+    resp = {
+        '400': {
+            "description": "Operation exception. Please check the "
+                           "response body for details."
+        },
+        '401': {
+            "description": "Unauthenticated access. Please login first."
+        },
+        '500': {
+            "description": "Unexpected error. Please check the "
+                           "response body for the stack trace."
+        }
+    }
+    if spec_respons is not None:
+        for resp_code in spec_respons:
+            resp[resp_code] = {"description": spec_respons.get(resp_code)}
+    return resp
+
+# Reduced version. Should include required or not
+def _gen_param(param, location):
     parameters = {
         'name': param['param_name'],
+        'in': location,
         'description': param['param_descr'],
-        'schema': { # Is it necessary to have 'schema'?
+        'schema': {
             'type': param['param_type']
         }
     }
     return parameters
-        
-# Reduced version. Should include all methods, security, etc. . 
-# Should return current values if None.
+
+      
+# Reduced version. Should include all methods, security, body_params, etc.
 def _gen_paths():
 
     paths = {}
-    for path in ENDPOINT_MAP:
+    for endpoint in ENDPOINT_MAP:
+
+        tag = ENDPOINT_MAP[endpoint][0]['tag']
+        if tag is "":
+            tag = "endpoint.method" # String for now since not defined for test-file
+        if tag not in API_TAGS:
+            API_TAGS[tag] = ""
+
+        spec_resp = ENDPOINT_MAP[endpoint][0]['respons']
+
+        # TODO: Make current values default. Add descr only if parameter exist. Both for query and path. 
         params = []
-        for p in ENDPOINT_MAP[path][0]['parameters'] :
-            params.append(_gen_param(p))
+        for p in ENDPOINT_MAP[endpoint][0]['parameters'] :
+            params.append(_gen_param(p, "query"))
 
         methods = {
             'get': {
-                'tags': [ENDPOINT_MAP[path][0]['tag']],
-                'summary': ENDPOINT_MAP[path][0]['summary'],
-                'responses': ENDPOINT_MAP[path][0]['respons'], # Not correct format yet.
+                'tags': [tag],
+                'summary': ENDPOINT_MAP[endpoint][0]['summary'],
+                'responses': _gen_responses_descr(spec_resp),
                 'parameters': params,
             }
         }        
-        paths[path] = methods
+        paths[endpoint] = methods
 
     return paths
 
@@ -158,10 +195,11 @@ def _gen_spec(all_endpoints=False, baseUrl=""):
 
 
 #-----------------To test/demonstrate----------------------
-print test_endpoint(42, "Hi!")
-print "------------------------------------"
-print _gen_tags()
-print "------------------------------------"
-print _gen_paths()
+first_endpoint(42, "1st endpoint")
+second_endpoint()
+# print "------------------------------------"
+# print _gen_tags()
+# print "------------------------------------"
+# print _gen_paths()
 print "------------------------------------"
 print _gen_spec()
