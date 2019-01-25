504f17742545c5c827c8c4c0abd28371b142a301
Tina Kallio <tina.kallio@gmail.com>
2019-01-25 10:56:01 +0100

    Dummy file to generate Open API spec (wip)
    Progress:
    Possible to generate group and group descriptions for controllers.
    Possible to generate description, responses and group for endpoints.
    Possible to generate parameters + descriptions.
    Possible to generate responses for endpoints.
    
    TODO:
    RequestBody parameters are not implemented. However, solution has
    been inmplement into Ceph code, so there is no need to do it here.
    Naming can be improved, but again, this will can be done directly in
    Ceph code.

--- api_doc_concept.py
+++ api_doc_concept.py
@@ -14,30 +14,45 @@ import cherrypy
 from distutils.util import strtobool
 import inspect
 
-# TODO: Parameters should work for simple types (int, etc) and dict (see _gen_body_param)
-# TODO: Add return-values (not the same as responses!)
-# TODO: (Make sure it works with other decorators (e.g. ApiController, Endpoint))
-# TODO: (Add more parameter types in later versions: [int], {[int]}, etc.)
-#-----------------in __init__----------------------
-       
-def EndpointDoc(descr="", group="", param={}, respons={}):
-    parameter_list = []
-    for param_name, param_info in param.items():
-        if isinstance(param_info, tuple):
-            parameter_list.append({
-                'param_name': param_name,
-                'param_type': param_info[0],
-                'param_descr': param_info[1],
-            })
-        else:
-            parameter_list.append({
-                'param_name': param_name,
-                'param_type': None,
-                'param_descr': "(No description available)",
-                'properties': param_info
-            })
+# TODO: Body parameters
+# TODO: Make prettier! Namn, for-loopar, osv.
 
-    for key in respons.keys():
+# TODO: (Make sure it works with other decorators (e.g. ApiController, Endpoint))
+# TODO: Alphabethical?
+# TODO: (Add more parameter types in later versions: [int], {[int]}, etc.)
+# TODO: func = endpoint.func or make a property? Why doesn't endpoint.func.doc_info work?
+# TODO: Make prettier!
+# TODO: Learn how to use logger properly...git status
+# TODO: Possibility to resuse parameters (e.g. fs_id) and resonses, using $ref?
+#-----------------in __init__----------------------
+def EndpointDoc(descr="", group="", param={}, body={}, respons={}):
+    
+    def _split(input):
+
+        res = [] # In order to make it consistent with e.g. default params of today
+        for key, value in input.items():
+            if isinstance(value, tuple):
+                res.append({
+                    'name': key,
+                    'type': value[0],
+                    'description': value[1]
+                })
+            elif isinstance(value, dict):
+                res.append({
+                    'name': key,
+                    'type': dict,
+                    'properties': _split(value)
+                })
+            else: # CFor e.g. lists. Not yet implemented.
+                res.append({
+                    'name': key,
+                    'type': object,
+                    'unknown_props': value
+                })
+        return res # TODO: naming 
+
+    for key, value in respons.items():
+        respons[key] = _split(value)
         if isinstance(key, int):
             respons[str(key)] = respons.pop(key)
 
@@ -45,7 +60,8 @@ def EndpointDoc(descr="", group="", param={}, respons={}):
         func.doc_info = {
             'summary': descr,
             'tag': group,
-            'parameters': parameter_list,
+            'parameters': _split(param),
+            'body': _split(body),
             'respons': respons
         }
         return func
@@ -71,11 +87,7 @@ class MyController():
         descr = "This is a dummy endpoint",
         param = {
             'my_num': (int, "A number of your choice"),
-            'my_string': (str, "A dummy message") 
-        },
-        respons = {
-            200: "OK",
-            '404': "Page not found"
+            'my_string': (str, "A dummy message")
         }
     )
     def first_endpoint(self, my_num, my_string):
@@ -86,13 +98,20 @@ class MySecondController():
     @EndpointDoc(
         descr = "This is a second dummy endpoint",
         respons = {
-            401: "Not the default message" 
+            200: {
+                'name': (str, 'Description of name'),
+                'age': (int, 'Description of age')
+            },
         },
-        param = {
+        body = {
             'user': {
-                'username': (str, 'desc'),
+                'username': {
+                    'nickname': (str, 'desc'),
+                    'realname': (str, 'desc')
+                },
                 'pass': (str, 'desc')
-            }
+            },
+            'colors': ["red, blue, yellow"]
         },
         group = "MyController"
     )
@@ -105,7 +124,7 @@ class MySecondController():
         return None
 
 
-# TODO: This will be done automatically (and differently) in __init__
+# This will be done automatically (and differently) in __init__
 ctrl_class = MyController()
 first_endpoint = ctrl_class.first_endpoint
 
@@ -127,7 +146,7 @@ class Docs():
     # Reduced version. Should check is_api, etc.
     @classmethod
     def _gen_tags(cls):
-        """ To generate a list of all tags in form [{name: description}] """
+        """ Generates a list of all tags and corresponding descriptions. """
         list_of_ctrl = set()
         for endpoints in ENDPOINT_MAP.values():
             for endpoint in endpoints:
@@ -153,7 +172,7 @@ class Docs():
 
     @classmethod
     def _get_tag(cls, endpoint): 
-        """ Returns the name of a tag to assign to a path """
+        """ Returns the name of a tag to assign to a path. """
         ctrl = endpoint.im_class
         tag = ctrl.__name__
         if hasattr(endpoint, 'doc_info') and endpoint.doc_info['tag']:
@@ -162,57 +181,108 @@ class Docs():
             tag = ctrl.doc_info['tag']
         return tag
 
-    # Reduced version. Should include all default responses.
+
     @classmethod
-    def _gen_responses(cls, spec_resp={}):
+    # isinstance doesn't work: param_type is always a <type 'type'>. TODO: better with ==?
+    def _type_to_str(cls, t): 
+        if t is str:
+            res = 'string'
+        elif t is int:
+            res = 'integer'
+        elif t is bool:
+            res = 'boolean'
+        elif t is list:
+            res = 'array'
+        elif t is float:
+            res = 'number'
+        else:
+            res = 'object'
+        return res # TODO: naming
+
+
+    @classmethod
+    def _add_info_to_param(cls, parameter, p_info):
+        for p in p_info:
+
+            if p['name'] == parameter['name'] and 'description' in p:
+                parameter['type'] = p['type']
+                parameter['description'] = p['description']
+            
+            if 'properties' in p:
+                parameter = cls._add_info_to_param(parameter, p['properties'])
+
+            # if 'unknown_props' in p:
+            #     pass
+        return parameter # TODO: naming
+
+    @classmethod
+    def _gen_content(cls, input):
+        properties = {}
+        for item in input:
+            properties[item['name']] = {
+                'type': cls._type_to_str(item['type']),
+                'description': item['description']
+            }
+            
+        content = {
+            'application/json': {
+                'schema': {
+                    'type': 'object',
+                    'properties': properties   
+                }
+            }
+        }
+        return content
+               
+    @classmethod
+    def _gen_responses(cls, method, return_objects={}):
         resp = {
             '400': {
                 "description": "Operation exception. Please check the "
-                            "response body for details."
+                               "response body for details."
             },
             '401': {
                 "description": "Unauthenticated access. Please login first."
             },
+            '403': {
+                "description": "Unauthorized access. Please check your "
+                               "permissions."
+            },
             '500': {
                 "description": "Unexpected error. Please check the "
-                            "response body for the stack trace."
+                               "response body for the stack trace."
             }
         }
+        if method.lower() == 'get':
+            resp['200'] = {'description': "OK"}
+        if method.lower() == 'post':
+            resp['201'] = {'description': "Resource created."}
+        if method.lower() == 'put':
+            resp['200'] = {'description': "Resource updated."}
+        if method.lower() == 'delete':
+            resp['204'] = {'description': "Resource deleted."}
+        if method.lower() in ['post', 'put', 'delete']:
+            resp['202'] = {'description': "Operation is still executing."
+                                          " Please check the task queue."}
 
-        for resp_code in spec_resp:
-            resp[resp_code] = {"description": spec_resp[resp_code]}
+        for code, content in return_objects.items(): # TODO: naming
+            resp[code].update({'content': cls._gen_content(content)})
+
         return resp
 
-    @classmethod
-    def _type_to_str(cls, param_type):
-        if isinstance(param_type, str):
-            res = 'string'
-        elif isinstance(param_type, int):
-            res = 'integer'
-        elif isinstance(param_type, bool):
-            res = 'boolean'
-        elif isinstance(param_type, list):
-            res = 'array'
-        elif isinstance(param_type, float):
-            res = 'number'
-        else:
-            res = 'object'
-        return res
 
     # Reduced version. Should include required or not
+    # TODO: This assumes primitive types. Should/does it work for arrays?
     @classmethod
     def _gen_param(cls, param, location):
         parameters = {
-            'name': param['param_name'],
+            'name': param['name'],
             'in': location,
-            'description': param['param_descr'],
+            'description': param['description'],
             'schema': {
-                'type': cls._type_to_str(param['param_type'])
+                'type': cls._type_to_str(param['type'])
             }
         }
-        if param.has_key('properties'):
-            pass
-            #TODO: Make this fit Open Api Spec
         return parameters
 
         
@@ -223,21 +293,23 @@ class Docs():
 
         for path, ep in sorted(list(ENDPOINT_MAP.items())):
             endpoint = ep[0]
-            
-            summary = "(No description available)"
+
             params = []
             if hasattr(endpoint, 'doc_info'):
                 if endpoint.doc_info['summary']:
                     summary = endpoint.doc_info['summary']
-                spec_resp = endpoint.doc_info['respons']
-                for p in endpoint.doc_info['parameters']:
-                    params.append(cls._gen_param(p, "query"))
+                resp = endpoint.doc_info['respons']
+                for param in endpoint.doc_info['parameters']: # TODO: naming
+                    params.append(cls._gen_param(param, "query"))
+            else: 
+                summary = "No description available"
+                resp = {} 
 
             methods = {
                 'get': {
                     'tags': [cls._get_tag(endpoint)],
                     'summary': summary,
-                    'responses': cls._gen_responses(spec_resp),
+                    'responses': cls._gen_responses('get', resp),
                     'parameters': params,
                 }
             }        
@@ -290,7 +362,7 @@ class Docs():
         }
 
         return spec
-
+        
 
 #-----------------To test/demonstrate----------------------
 print Docs()._gen_spec()
